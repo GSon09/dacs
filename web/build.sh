4d3386ea4984f778d5fdc85035e0299016264ba5
Phonglaai <22010011@st.phenikaa-uni.edu.vn>
2025-11-19 22:22:14 +0700

    Add Render deployment config

---
+++ web/build.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+# exit on error
+set -o errexit
+
+composer install --no-dev --optimize-autoloader
+
+php artisan config:cache
+php artisan route:cache
+php artisan view:cache
+
+npm ci
+npm run build
